fa621e969c556c069726c12786c6be694286e663
sudhanshuvishwakarm <ramkumarvishwakarma2005@gmail.com>
2025-09-29 16:05:21 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        esmExternals: true, // May help with ESM compatibility
+    },
+};
 
 export default nextConfig;
+
